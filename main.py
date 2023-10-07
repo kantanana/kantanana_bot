20e7ef7077340e2a74cc658552b45977fc770a29
kantanana <85265596+kantanana@users.noreply.github.com>
2023-10-07 07:16:40 +0000

    fixed some serious security loopholes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 from log import logger
 from handler import start_handler, echo_handler, duck_handler
-
+import os
+telebot_key = os.environ['TELEBOT']
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('6674542557:AAHWwRG6kuCChqkhzExRqpeVfgLczM20hUg').build()
+    application = ApplicationBuilder().token(telebot_key).build()
 
     application.add_handler(start_handler())
     application.add_handler(echo_handler())
