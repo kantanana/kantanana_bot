25f9a778bd0650cb215182690be42908eda5239b
kantanana <85265596+kantanana@users.noreply.github.com>
2023-10-08 02:26:09 +0000

    added previous code in comments

--- commands.py
+++ commands.py
@@ -1,7 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 import requests
-# from gpt import question
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -15,7 +14,7 @@ async def duck(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with open('duck.png', 'w') as f:
         f.write(img)
     await context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=open('duck.png', 'rb'))
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text='https://random-d.uk/api/v2/randomimg')
+
     
 
-# async def chatgpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await context.bot.send_message(chat_id=update.effective_chat.id, text=question(update.message.text))
